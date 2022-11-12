411ab23c51cb2f412bc7f8fc60331dc438d324a6
Jeff Chang <aaron.o.scherling@gmail.com>
2022-11-12 10:32:30 -0600

    create repo

---
+++ vite.config.ts
@@ -0,0 +1,43 @@
+import path from 'path';
+
+import { visualizer } from 'rollup-plugin-visualizer';
+import solid from 'solid-start/vite';
+import { defineConfig } from 'vite';
+// import checker from 'vite-plugin-checker'; // add when solid start adds strict ts config
+import EnvironmentPlugin from 'vite-plugin-environment';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+export default defineConfig({
+  build: {
+    chunkSizeWarningLimit: 1000,
+    emptyOutDir: true,
+    outDir: path.join(__dirname, 'dist'),
+  },
+  plugins: [
+    solid(),
+    visualizer(),
+    tsconfigPaths(),
+    EnvironmentPlugin('all', { prefix: 'WEB_' }),
+    // checker({
+    //   eslint: {
+    //     lintCommand: 'eslint --ignore-pattern "**/*.test.*"  --cache --ext ts,tsx "./src/"',
+    //   },
+    //   typescript: true,
+    // }),
+  ],
+  server: {
+    hmr: true,
+    port: 3000,
+  },
+  test: {
+    clearMocks: true,
+    environment: 'jsdom',
+    exclude: ['**/node_modules/**', '**/dist/**', '**/.{idea,git,cache,output,temp}/**', '**/mocks/**'],
+    globals: true,
+    include: ['src/**/?(*.)(test).[jt]s?(x)'],
+    outputFile: {
+      html: './reports/index.html',
+    },
+    setupFiles: ['./configs/test/setupTests.js'],
+  },
+});
