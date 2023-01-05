1729af666d9eacd266b53e46ff5e8afba72692dd
TheAsianNextDoor <aaron.o.scherling@gmail.com>
2023-01-05 13:18:56 -0600

    Add photo panel and aws SDK

--- src/features/task-actions/task-photos/photos-panel.tsx
+++ src/features/task-actions/task-photos/photos-panel.tsx
@@ -1,4 +1,11 @@
-import { createSignal } from 'solid-js';
+import { LinearProgress } from '@suid/material';
+import { Show, createSignal } from 'solid-js';
+import { createServerAction$ } from 'solid-start/server';
+
+import { Button } from 'components/lib/button';
+import { AWS_SDK } from 'utils/aws-sdk';
+
+import { PhotoList } from './photo-list';
 
 import type { TaskWithLinks } from 'server/task/task-types';
 import type { Component } from 'solid-js';
@@ -7,27 +14,61 @@ interface props {
   task: TaskWithLinks;
 }
 
+const user = 'aaron';
+
 export const PhotosPanel: Component<props> = (props) => {
-  const [selectedFile, setSelectedFile] = createSignal();
+  const [selectedFiles, setSelectedFiles] = createSignal<FileList>();
+  const [progress, setProgress] = createSignal(0);
+
+  const [uploadUrl, getUploadUrl] = createServerAction$(async (params: string[]) => {
+    return AWS_SDK.s3.generatePreSignedPutUrl(params);
+  });
 
   const photoSelectHandler = (event: Event) => {
     const target = event.target as HTMLInputElement;
+    setProgress(0);
 
-    setSelectedFile(target.files);
+    if (target.files) {
+      setSelectedFiles(target.files);
+    }
   };
 
-  const handleUpload = () => {
-    console.log('photo: ', selectedFile());
+  const handleUpload = async () => {
+    if (!selectedFiles() && !selectedFiles()?.length) {
+      return;
+    }
+
+    const fileList = selectedFiles() as FileList;
+    const uploadArray = [];
+
+    for (let i = 0; i < fileList.length; i += 1) {
+      const file = fileList.item(i) as File;
+
+      uploadArray[i] = `images/${user}/${file.name}`;
+    }
+
+    await getUploadUrl(uploadArray);
+
+    const signedUrls = uploadUrl.result as string[];
+    signedUrls.forEach((url, i) => fetch(url, { method: 'PUT', body: fileList[i] }));
   };
 
   return (
     <>
-      <div>Photos</div>
+      <div>Upload a Photo</div>
       <div>
         <input type="file" accept="image/*" multiple name="photoUpload" onChange={photoSelectHandler} />
       </div>
+      <Show when={progress() !== 0}>
+        <LinearProgress variant="determinate" value={progress()} />
+      </Show>
+      <div class="pt-5">
+        <Button variant="contained" onClick={handleUpload}>
+          Upload Photo
+        </Button>
+      </div>
       <div>
-        <button onClick={handleUpload}>Upload Photo</button>
+        <PhotoList />
       </div>
     </>
   );
---
+++ src/utils/aws-sdk.ts
@@ -0,0 +1,93 @@
+import { GetObjectCommand, ListObjectsV2Command, PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
+
+const accessKeyId = process.env.ACCESS_KEY_ID as string;
+const secretAccessKey = process.env.SECRET_ACCESS_KEY as string;
+const Bucket = process.env.S3_BUCKET as string;
+
+const s3Client = new S3Client({
+  region: 'us-east-1',
+  credentials: {
+    accessKeyId,
+    secretAccessKey,
+  },
+});
+
+const generatePreSignedPutUrl = async (pathToFileArray: string[]) => {
+  if (!pathToFileArray || !pathToFileArray.length) {
+    return [];
+  }
+
+  const urlArray = [];
+
+  try {
+    for (let i = 0; i < pathToFileArray.length; i += 1) {
+      const filePath = pathToFileArray[i];
+
+      const command = new PutObjectCommand({
+        Bucket,
+        Key: filePath,
+      });
+
+      // eslint-disable-next-line no-await-in-loop
+      const url = await getSignedUrl(s3Client, command);
+
+      urlArray[i] = url;
+    }
+  } catch (e) {
+    console.error(e);
+  }
+
+  return urlArray;
+};
+
+const generatePreSignedGetUrl = async (pathToFileArray: string[]) => {
+  if (!pathToFileArray || !pathToFileArray.length) {
+    return [];
+  }
+
+  const urlArray = [];
+
+  try {
+    for (let i = 0; i < pathToFileArray.length; i += 1) {
+      const filePath = pathToFileArray[i];
+
+      const command = new GetObjectCommand({
+        Bucket,
+        Key: filePath,
+      });
+
+      // eslint-disable-next-line no-await-in-loop
+      const url = await getSignedUrl(s3Client, command);
+
+      urlArray[i] = url;
+    }
+  } catch (e) {
+    console.error(e);
+  }
+
+  return urlArray;
+};
+
+const getObjectsFromFolder = async (folderPath: string) => {
+  const data = await s3Client.send(new ListObjectsV2Command({ Bucket: process.env.WEB_S3_BUCKET, Prefix: folderPath }));
+
+  return data;
+};
+
+const generatePreSignedGetUrlFromFolder = async (folderPath: string) => {
+  const data = await getObjectsFromFolder(folderPath);
+
+  const pathsToFiles = data?.Contents?.map((file) => file.Key) as string[];
+
+  return generatePreSignedGetUrl(pathsToFiles);
+};
+
+export const AWS_SDK = {
+  s3: {
+    getObjectsFromFolder,
+    generatePreSignedGetUrl,
+    generatePreSignedPutUrl,
+    generatePreSignedGetUrlFromFolder,
+  },
+};
