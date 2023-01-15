cf4786b347f4e2166f2a03825e0e8793d2dbe8ac
TheAsianNextDoor <aaron.o.scherling@gmail.com>
2023-01-15 14:07:00 -0600

    only send typing event on non enter keys

--- src/features/task-actions/task-comments/comment-input.tsx
+++ src/features/task-actions/task-comments/comment-input.tsx
@@ -1,7 +1,7 @@
 import { createSignal } from 'solid-js';
 
 import { TextField } from '~/components/lib/text-field';
-import { api } from '~/utils/trpc';
+import { trpcClient } from '~/utils/trpc';
 
 import type { Component } from 'solid-js';
 import type { CommentWithUser } from '~/server/db/types/comment-types';
@@ -14,8 +14,8 @@ interface props {
 }
 
 export const CommentInput: Component<props> = (props) => {
-  const { mutate: addComment, error: addCommentError } = api.comment.create.useMutation();
-  const { mutate: typingUser } = api.comment.userTyping.useMutation();
+  const { mutate: addComment, error: addCommentError } = trpcClient.comment.create.useMutation();
+  const { mutate: typingUser } = trpcClient.comment.userTyping.useMutation();
   const [inputRef, setInputRef] = createSignal<HTMLInputElement | Record<string, unknown>>({});
 
   const channel = pusherClient.subscribe('typing-users');
@@ -24,12 +24,13 @@ export const CommentInput: Component<props> = (props) => {
     const target = e.target as HTMLInputElement;
     const lastComment = () => props.comments[props.comments.length - 1]?.id;
 
-    typingUser({
-      userId: props.task.userId as string,
-      taskId: props.task.id,
-      userName: 'aaron',
-    });
-
+    if (e.key !== 'Enter') {
+      typingUser({
+        userId: props.task.userId as string,
+        taskId: props.task.id,
+        userName: 'aaron',
+      });
+    }
     if (e.key === 'Enter') {
       addComment({
         message: target.value,
