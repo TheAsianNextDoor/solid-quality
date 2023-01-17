d172ea1dcd4478eeadac579f0c43dfe66eb3710c
TheAsianNextDoor <aaron.o.scherling@gmail.com>
2023-01-16 21:37:17 -0600

    add easy links on index

---
+++ src/routes/index.tsx
@@ -0,0 +1,14 @@
+import { A } from 'solid-start';
+
+export default function Index() {
+  return (
+    <>
+      <div>
+        <A href="/login">Login</A>
+      </div>
+      <div>
+        <A href="/inspection">Inspection</A>
+      </div>
+    </>
+  );
+}
