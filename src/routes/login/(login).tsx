55b523c0570204f0bb16d8c1968e127a8e54fe0d
TheAsianNextDoor <aaron.o.scherling@gmail.com>
2023-02-13 00:07:15 -0600

    fix login page from deps bump

--- src/routes/login/(login).tsx
+++ src/routes/login/(login).tsx
@@ -1,27 +1,26 @@
-import { getSession } from '@auth/solid-start';
+// import { getSession } from '@auth/solid-start';
 import { signIn, signOut } from '@auth/solid-start/client';
 import { Switch, Match } from 'solid-js';
-import { Title, useRouteData } from 'solid-start';
-import { createServerData$ } from 'solid-start/server';
+import { Title } from 'solid-start';
+// import { createServerData$ } from 'solid-start/server';
 
-import { authOpts } from '~/routes/api/auth/[...solidauth]';
+// import { authOpts } from '~/routes/api/auth/[...solidauth]';
 import { trpcClient } from '~/utils/trpc';
 
 import type { VoidComponent } from 'solid-js';
 
-export const routeData = () => {
-  return createServerData$(async (_, { request }) => {
-    return getSession(request, authOpts);
-  });
-};
+// export const routeData = () => {
+//   return createServerData$(async (_, { request }) => {
+//     const session = await getSession(request, authOpts);
+//     console.log('blah: ', session);
+
+//     return session;
+//   });
+// };
 
 const Home: VoidComponent = () => {
-  const session = useRouteData<typeof routeData>();
-  const res = trpcClient.session.get.useQuery(undefined, {
-    get enabled() {
-      return !!session()?.user;
-    },
-  });
+  // const session = useRouteData<typeof routeData>();
+  const res = trpcClient.session.get.useQuery();
 
   return (
     <>
@@ -32,8 +31,19 @@ const Home: VoidComponent = () => {
             <div class="text-2xl font-bold text-gray-500">{res.isFetching ? 'Loading' : 'Not Logged In'}</div>
           </Match>
         </Switch>
-        <Switch
-          fallback={
+        <Switch>
+          <Match when={res.isLoading}>
+            <h1>Loading session</h1>
+          </Match>
+          <Match when={res?.data?.user?.name}>
+            <button
+              onClick={() => signOut()}
+              class="m-3 flex w-56 items-center justify-center rounded-lg bg-purple-700 p-2.5 font-bold text-white"
+            >
+              Logout
+            </button>
+          </Match>
+          <Match when={!res?.data?.user?.name}>
             <>
               <button
                 onClick={() => signIn('github')}
@@ -48,18 +58,6 @@ const Home: VoidComponent = () => {
                 Login Google
               </button>
             </>
-          }
-        >
-          <Match when={session.loading}>
-            <h1>Loading session</h1>
-          </Match>
-          <Match when={session()}>
-            <button
-              onClick={() => signOut()}
-              class="m-3 flex w-56 items-center justify-center rounded-lg bg-purple-700 p-2.5 font-bold text-white"
-            >
-              Logout
-            </button>
           </Match>
         </Switch>
       </div>
