1c42b8c19757f39301ab46dc47f295c94a19035d
TheAsianNextDoor <aaron.o.scherling@gmail.com>
2023-02-14 11:54:58 -0600

    attempt at generic trpc options type

---
+++ src/types/trpc-types.ts
@@ -0,0 +1,22 @@
+// import type { AnyProcedure, inferProcedureInput, inferProcedureOutput } from '@trpc/server';
+// import type { TRPCClientErrorLike } from 'solid-trpc';
+// import type { UseTRPCMutationOptions, UseTRPCQueryOptions } from 'solid-trpc/dist/shared';
+// import type { IAppRouter } from '~/server/trpc/router/_app';
+
+/**
+ * DO NOT USE HERE FOR HISTORICAL PURPOSES
+ */
+
+// export type TrpcMutationOptions = UseTRPCMutationOptions<
+//   inferProcedureInput<AnyProcedure>,
+//   TRPCClientErrorLike<AnyProcedure>,
+//   inferProcedureOutput<AnyProcedure>
+// >;
+
+// export type TrpcQueryOptions<path extends string> = UseTRPCQueryOptions<
+//   path,
+//   inferProcedureInput<IAppRouter['task']['getTasksByInspection']>,
+//   inferProcedureOutput<IAppRouter['task']['getTasksByInspection']>,
+//   inferProcedureOutput<IAppRouter['task']['getTasksByInspection']>,
+//   TRPCClientErrorLike<IAppRouter['task']['getTasksByInspection']>
+// >;
