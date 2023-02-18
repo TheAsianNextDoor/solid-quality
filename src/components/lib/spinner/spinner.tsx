804b1b2e29a47980af7b0a7fec6860a844613b44
TheAsianNextDoor <aaron.orlando.scherling@gmail.com>
2023-02-17 18:09:12 -0600

    fix build issue

--- src/components/lib/spinner/spinner.tsx
+++ src/components/lib/spinner/spinner.tsx
@@ -1,7 +1,9 @@
-import { CircularProgress } from '@suid/material';
+// import { CircularProgress } from '@suid/material';
 
 import type { Component } from 'solid-js';
 
 export const Spinner: Component<any> = (props) => {
-  return <CircularProgress {...props} />;
+  // return <CircularProgress {...props} />;
+
+  return <>Loading...</>;
 };
